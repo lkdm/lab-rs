672794e29f57ee432f9f1cc443bb20a75ad047a9
Luke M <44519853+lkdm@users.noreply.github.com>
2024-03-01 16:46:46 +1100

    feat (wip): compound interest calculator.

--- compound-interest/src/lib.rs
+++ compound-interest/src/lib.rs
@@ -1,3 +1,6 @@
 #[macro_use]
 mod currency;
 pub use currency::Currency;
+
+mod calculator;
+pub use calculator::{AnnualFrequency, CompoundInterest, FinancialCalculator};
--- compound-interest/src/main.rs
+++ compound-interest/src/main.rs
@@ -1,30 +1,24 @@
 #[macro_use]
 mod macros;
 use compound_interest::Currency;
+use compound_interest::{AnnualFrequency, CompoundInterest, FinancialCalculator};
 use rust_decimal_macros::dec;
 
 fn main() {
-    // let mut principle = Currency(dec!(41303.15)) * dec!(0.01);
-    // let mut principle = currency!(41303.15) * dec!(0.05);
-    // println!("{}", principle);
-    let mut p = currency!(51303.15);
-    let r = dec!(0.05) / dec!(12) + dec!(1);
-    let mut total_interest = currency!(0);
-    let monthly_deposit = currency!(4_000);
+    let compound_interest = CompoundInterest::new(
+        currency!(50_000),
+        dec!(0.053),
+        currency!(1_000),
+        AnnualFrequency::Weekly,
+        AnnualFrequency::Monthly,
+        48,
+    );
+    let result = compound_interest.calculate().unwrap();
 
-    for month in 0..17 {
-        if month == 2 {
-            p -= currency!(20_000);
-        }
-        if month == 4 {
-            p -= currency!(10_000);
-        }
-        p += monthly_deposit;
-        let i = p * r - p;
-        total_interest += i;
-        p += i; // Add the interest to the principal for compounding
+    for (i, value) in result.iter().enumerate() {
+        println!("{}", value);
     }
 
-    println!("Total Interest over 2 years: {}", total_interest);
-    println!("New principle: {}", p)
+    // println!("Total Interest over 2 years: {}", total_interest);
+    // println!("New principle: {}", p)
 }
